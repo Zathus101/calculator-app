6e535c1d4a0a8598050960bf16e8590736ae72b1
Zaki M <zmirimir@gmu.edu>
2023-06-20 19:39:25 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="area" >
+      <div class="number">
+        <div class="digits">
+          0
+        </div>
+      </div>
+      <div class="bot-btns">
+          <div id="btn-row1">
+              <Button>AC</Button>
+              <Button>+/-</Button>
+              <Button>%</Button>
+              <Button>÷</Button>
+          </div>
+          <div id="btn-row2">
+              <Button>7</Button>
+              <Button>8-</Button>
+              <Button>9</Button>
+              <Button>x</Button>
+          </div>
+          <div id="btn-row3">
+              <Button>4</Button>
+              <Button>5</Button>
+              <Button>6</Button>
+              <Button>-</Button>
+          </div>
+          <div id="btn-row4">
+              <Button>1</Button>
+              <Button>2</Button>
+              <Button>3</Button>
+              <Button>+</Button>
+          </div>
+          <div id="btn-row5">
+              <Button>0</Button>
+              <Button>.</Button>
+              <Button>=</Button>
+              
+          </div>
+      </div>
     </div>
   );
 }
